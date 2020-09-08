39185a58d8182f3dc0c00a05e40cc131fc8f54a8
Hadi <49832502+mhadi813@users.noreply.github.com>
2020-09-08 12:23:55 -0400

    Update demo setup.sh

--- demo setup.sh
+++ demo setup.sh
@@ -9,7 +9,14 @@ cd $CONDA_PREFIX
 mkdir -p ./etc/conda/activate.d
 mkdir -p ./etc/conda/deactivate.d
 touch ./etc/conda/activate.d/env_vars.sh
+# set environment variables in activation script
+#!/bin/sh
+# export GOOGLE_APPLICATION_CREDENTIALS='path/to/service-account-file.json'
+
 touch ./etc/conda/deactivate.d/env_vars.sh
+# unset environment variables in deactivate script
+#!/bin/sh
+# unset GOOGLE_APPLICATION_CREDENTIALS
 
 # launch jupter lab
 jupyter lab
